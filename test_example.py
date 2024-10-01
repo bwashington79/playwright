f9e17828c2f49696af5e47a63bbf147ff462d371
Bobby-Washington <bobwashington@hotmail.com>
2024-10-01 09:06:04 -0400

    learning

--- test_example.py
+++ test_example.py
@@ -1,10 +1,16 @@
 import re
 from playwright.sync_api import Page, expect
 
-msg = "Foo"
 
 def test_has_title(page: Page):
         page.goto("https://playwright.dev/")
-        print(msg)
-        # Expect a title "to contain" a substring foo.
+
+# Expect a title "to contain" a substring.
         expect(page).to_have_title(re.compile("Playwright"))
+
+def test_get_started_link(page: Page):
+        page.goto("https://playwright.dev/foo")
+
+# Click the get started link.
+        page.get_by_role("link", name="Get started").click()
+
